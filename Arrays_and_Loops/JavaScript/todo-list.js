a029bc05be814967820275b34ec4369bb585577f
devlou-rens <lourensgacias00@gmail.com>
2026-05-02 00:21:46 +0800

    Add: Todo List HTML-CSS-JS

---
+++ Arrays_and_Loops/JavaScript/todo-list.js
@@ -0,0 +1,52 @@
+const todoList = [{
+    name:'Watching YT', 
+    dueDate:'2026-12-22'},
+    {
+    name:'Cooking dinner', 
+    dueDate:'2026-12-22'}];
+
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++){
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name } = todoObject;
+        const { dueDate } = todoObject;
+        const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+            " class="btn-delete">Delete</button>
+            `;
+        todoListHTML += html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+const addTodo = () => {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    const dueDateElement = document.querySelector('.js-date-input');
+    const dueDate = dueDateElement.value;
+
+    todoList.push(
+        {//name: name, // passing the inputElement to array[] using .push
+        //dueDate: dueDate}
+        name, dueDate});
+    inputElement.value = '';
+    dueDateElement.value = '';
+
+    renderTodoList();
+}
+
+
+
+
+
